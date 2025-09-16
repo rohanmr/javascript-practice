cf147b87beb28fda258fe2c7a83d2ac706d88a6c
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2025-09-16 23:25:38 +0530

    added some js practice programs

---
+++ pattern/pattern.js
@@ -0,0 +1,28 @@
+//  *
+//  * *
+//  * * *
+//  * * * *
+//  * * * * *
+
+for (let i = 1; i <= 5; i++) {
+    let line = "";
+    for (let j = 1; j <= i; j++) {
+        line += " *";
+    }
+    console.log(line);
+}
+
+//  * * * * *
+//  * * * *
+//  * * *
+//  * *
+//  *
+
+
+for (i = 1; i <= 5; i++) {
+    line = ""
+    for (j = 5; j >= i; j--) {
+        line = line + " *"
+    }
+    console.log(line)
+}
